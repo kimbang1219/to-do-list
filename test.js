155713fc764f8c793643c5d158b88c5d69220f3d
kimbang1219 <71418870+kimbang1219@users.noreply.github.com>
2022-08-05 18:10:53 +0900

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-console.log(document.getElementById("test").innerHTML)
-console.log(document.getElementById("test").innerText)
-console.log(document.getElementById("test").textContent)
-
-//document.getElementById("test").innerHTML=`<h1>Noona</h1>`
-document.getElementById("test").textContent=`<h1>Noona</h1>`
-//console.log(document.getElementById("test").textConent=`<h1>Noona</h1>`)
